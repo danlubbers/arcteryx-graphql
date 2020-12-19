7b7db4886ee09c42c623f050771ec0e50a505e0d
Dan Lubbers <lubbers.dan@gmail.com>
2020-12-19 16:20:41 -0500

    Added Typescript and converted App.js to tsx

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -20,6 +20,7 @@ const Product = (props) => {
   const { product } = useContentful(query, props.match.params.slug);
   const [jacketColor, setJacketColor] = useState(null);
   const [color, setColor] = useState(null);
+
   useEffect(() => {
     setJacketColor(product && product.imagesCollection.items[1].url);
     setColor(product && product.imagesCollection.items[1].description);
