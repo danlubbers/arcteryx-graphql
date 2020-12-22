0a48b2ca2cf093ee55df37ca0ab6f7c779baa3b1
Dan Lubbers <lubbers.dan@gmail.com>
2020-12-21 19:44:44 -0500

    Added mens and womens category in header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Header/Header.module.scss";
 import logo from "../../assets/logo/arc-teryx.svg";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [showNavList, setShowNavList] = useState(false);
   return (
-    <div className={styles.headerContainer}>
+    <header className={styles.headerContainer}>
       <Link to="/">
         <img className={styles.logo} src={logo} alt="logo" />
       </Link>
       <nav className={styles.navbarWrapper}>
-        <Link to="/products">
-          <p className={styles.productsText}>Products</p>
-        </Link>
+        <p
+          className={styles.productsText}
+          onClick={() => setShowNavList(!showNavList)}
+        >
+          Products
+        </p>
+        {showNavList && (
+          <ul className={styles.navListWrapper}>
+            <Link to="/products">
+              <li>Mens</li>
+            </Link>
+            <li>Womens</li>
+          </ul>
+        )}
         <Link to="/search">
           <FaSearch className={styles.search} />
         </Link>
       </nav>
-    </div>
+    </header>
   );
 }
 
