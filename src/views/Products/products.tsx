7bb9d7b7af87d78f68107b797a4c70dcf452e816
Dan Lubbers <lubbers.dan@gmail.com>
2020-12-24 14:33:26 -0500

    Split products so the rendering content is in it's own component

--- src/views/Products/products.tsx
+++ src/views/Products/products.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import styles from "../Products/products.module.scss";
 import useContentful from "../../hooks/use-contentful";
-import { Link } from "react-router-dom";
 import { query } from "../../utils/contentful-query";
 import { queryProps } from "../../utils/contentful-query-props";
 import Header from "../../Components/Header/Header";
 import Loading from "../../Components/Loading/Loading";
+import RenderProducts from "../../Components/RenderProducts/RenderProducts";
 
 const Products = (props: { location: { pathname: string } }) => {
   const pathnameGender = props.location.pathname.split("/")[2];
   const pathnameCategory = props.location.pathname.split("/")[3];
   const { products } = useContentful(query, null);
 
-  const filterProductsByGender = products
+  const filteredProducts = products
     .filter((product: queryProps) => {
       return product.gender === pathnameGender || product.gender === "unisex";
     })
@@ -26,24 +25,7 @@ const Products = (props: { location: { pathname: string } }) => {
       {!products ? (
         <Loading />
       ) : (
-        <div className={styles.productsContainer}>
-          {filterProductsByGender
-            .map((product: queryProps, index: number) => {
-              return (
-                <div key={`products-${index}`}>
-                  <Link to={`/product/${product.slug}`}>
-                    <p className={styles.title}>{product.title}</p>
-                    <img
-                      className={styles.productImage}
-                      src={product.imagesCollection.items[0].url}
-                      alt={product.title}
-                    />
-                  </Link>
-                </div>
-              );
-            })
-            .reverse()}
-        </div>
+        <RenderProducts filteredProducts={filteredProducts} />
       )}
     </>
   );
