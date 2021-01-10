ea6371605cc00e321bc4302ef09e8b5207ef9adb
Dan Lubbers <lubbers.dan@gmail.com>
2021-01-10 15:17:38 -0500

    Fixed Jest router issue. In the test file, the Component has to be wrapped with Router.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
 import { render } from "@testing-library/react";
 import App from "./App";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 test("Parent Component", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
   expect(getByText("Header")).toEqual(true);
 });
--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,7 +1,15 @@
 import { render, fireEvent } from "@testing-library/react";
 import Header from "./Header";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 test("user clicked mens", () => {
-  const { getByText } = render(<Header />);
+  const { getByText } = render(
+    <Router history={history}>
+      <Header />
+    </Router>
+  );
   fireEvent.click(getByText("Mens"));
 });
