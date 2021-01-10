55d44ca2f7f6b6a256cd2f941c01d4f996f6f85c
Dan Lubbers <lubbers.dan@gmail.com>
2021-01-10 15:04:38 -0500

    Changes to routes for jest broke the routes in the app. Revereverted back to original code for route issue.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import PWAModal from "./Components/PWAModal/PWAModal";
 import Modal from "./Components/Modal/Modal";
 import useIsIOS from "./utils/useIsIOS";
 
-import { BrowserRouter as Router } from "react-router-dom";
-
 interface AppProps {
   location: {
     pathname: string;
@@ -24,19 +22,17 @@ const App: React.FC<AppProps> = (props) => {
   };
 
   return (
-    <Router>
-      <div className="App">
-        <Header location={pathname} />
+    <div className="App">
+      <Header location={pathname} />
 
-        {prompt && !openModal ? (
-          <PWAModal handleModalClick={handleModalClick} />
-        ) : (
-          <Modal />
-        )}
+      {prompt && !openModal ? (
+        <PWAModal handleModalClick={handleModalClick} />
+      ) : (
+        <Modal />
+      )}
 
-        <div className="backgroundImage"></div>
-      </div>
-    </Router>
+      <div className="backgroundImage"></div>
+    </div>
   );
 };
 
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,9 +4,6 @@ import logo from "../../assets/logo/arc-teryx.svg";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import HeaderDropdown from "../HeaderDropdown/HeaderDropdown";
-
-import { BrowserRouter as Router } from "react-router-dom";
-
 interface HeaderProps {
   location?: string;
 }
@@ -21,48 +18,46 @@ const Header: React.FC<HeaderProps> = ({ location }) => {
   }, [location]);
 
   return (
-    <Router>
-      <main>
-        <Link to="/" aria-label="arcteryx logo">
-          <img
-            className={styles.logo}
-            src={logo}
-            alt="arcteryx logo"
+    <main>
+      <Link to="/" aria-label="arcteryx logo">
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="arcteryx logo"
+          onClick={() => {
+            setShowMensNavList(false);
+            setShowWomensNavList(false);
+          }}
+        />
+      </Link>
+      <nav>
+        <div className={styles.productsWrapper}>
+          <p
+            className={styles.productsText}
             onClick={() => {
-              setShowMensNavList(false);
+              setShowMensNavList(!showMensNavList);
               setShowWomensNavList(false);
             }}
-          />
+          >
+            Mens
+          </p>
+          {showMensNavList && <HeaderDropdown gender="mens" />}
+          <p
+            className={styles.productsText}
+            onClick={() => {
+              setShowWomensNavList(!showWomensNavList);
+              setShowMensNavList(false);
+            }}
+          >
+            Womens
+          </p>
+          {showWomensNavList && <HeaderDropdown gender="womens" />}
+        </div>
+        <Link to="/search" aria-label="search">
+          <FaSearch className={styles.search} />
         </Link>
-        <nav>
-          <div className={styles.productsWrapper}>
-            <p
-              className={styles.productsText}
-              onClick={() => {
-                setShowMensNavList(!showMensNavList);
-                setShowWomensNavList(false);
-              }}
-            >
-              Mens
-            </p>
-            {showMensNavList && <HeaderDropdown gender="mens" />}
-            <p
-              className={styles.productsText}
-              onClick={() => {
-                setShowWomensNavList(!showWomensNavList);
-                setShowMensNavList(false);
-              }}
-            >
-              Womens
-            </p>
-            {showWomensNavList && <HeaderDropdown gender="womens" />}
-          </div>
-          <Link to="/search" aria-label="search">
-            <FaSearch className={styles.search} />
-          </Link>
-        </nav>
-      </main>
-    </Router>
+      </nav>
+    </main>
   );
 };
 
