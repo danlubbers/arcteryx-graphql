bb0306493c7e80ee810fea7523393ff68274fb21
Dan Lubbers <lubbers.dan@gmail.com>
2021-01-11 16:37:55 -0500

    Added the PWA service workers.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
 
 import Routes from "./routes/routes";
 import { Router } from "react-router-dom";
@@ -24,3 +25,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
