65fddbcf546c13ee3c4bfa2a9cbdc60ebefbd523
Dan Lubbers <lubbers.dan@gmail.com>
2021-01-15 18:19:56 -0500

    Refactored Header test file

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -4,32 +4,25 @@ import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
+const renderWithRouter = (component) => {
+  return {
+    ...render(<Router history={history}>{component}</Router>),
+  };
+};
 
 test("user clicked Mens", () => {
-  const { getByText } = render(
-    <Router history={history}>
-      <Header />
-    </Router>
-  );
+  const { getByText } = renderWithRouter(<Header />);
   fireEvent.click(getByText("Mens"));
 });
 
 test("user clicked Womens", () => {
-  const { getByText } = render(
-    <Router history={history}>
-      <Header />
-    </Router>
-  );
+  const { getByText } = renderWithRouter(<Header />);
   fireEvent.click(getByText("Womens"));
 });
 
 test("user clicked Search Icon", () => {
   history.push = jest.fn();
-  const { getByTestId } = render(
-    <Router history={history}>
-      <Header />
-    </Router>
-  );
+  const { getByTestId } = renderWithRouter(<Header />);
   fireEvent.click(getByTestId("search"));
   expect(history.push).toHaveBeenCalledWith("/search");
 });
